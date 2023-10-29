34f62292adf865e49d8b0f3b2b29ab8c6d81a189
Seoyoon <wideskyinme@gmail.com>
2023-10-29 16:50:04 +0900

    init commit

---
+++ src/main/java/com/example/springassignmentlv2/SpringAssignmentLv2Application.java
@@ -0,0 +1,13 @@
+package com.example.springassignmentlv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAssignmentLv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAssignmentLv2Application.class, args);
+	}
+
+}
